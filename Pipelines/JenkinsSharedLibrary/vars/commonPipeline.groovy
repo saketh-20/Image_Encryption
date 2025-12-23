af3cd293e9cb76102a7bf43f26cdb3eae5908147
Saketh Chowdari <y.saketh20@gmail.com>
2025-12-23 17:30:57 +0530

    jenkins integrated

---
+++ Pipelines/JenkinsSharedLibrary/vars/commonPipeline.groovy
@@ -0,0 +1,34 @@
+def call(Map config = [:]) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git url: config.repoUrl, branch: config.branch
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    echo 'Build step'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    echo 'Test step'
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    echo 'Deploy step'
+                }
+            }
+
+        }
+    }
+}
